3c5c27c32c256a5c21e05017d90a220adba2f62b
heinGertenbach <71070518+heinGertenbach@users.noreply.github.com>
2021-04-01 14:54:35 +0200

    trying

--- src/main/java/co/za/bonk/DiscordCLientUser.java
+++ src/main/java/co/za/bonk/DiscordCLientUser.java
@@ -7,6 +7,9 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.rest.entity.RestChannel;
+import discord4j.rest.entity.RestGuild;
+import discord4j.common.util.Snowflake;
 
 public class DiscordCLientUser implements Runnable {
 
@@ -24,11 +27,16 @@ public class DiscordCLientUser implements Runnable {
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
+        RestChannel Testchannel = client.getChannelById(Snowflake.of("827103671233937408"));
+        Testchannel.createMessage("wow");
+
         gateway.on(MessageCreateEvent.class).subscribe(event -> {
             final Message message = event.getMessage();
             final String contents = message.getContent();
 
-            if(contents.startsWith(prefix)) {
+            message.getChannel().block().createMessage("wow");
+
+            if(contents.startsWith("$")) {
                 final MessageChannel channel = message.getChannel().block();
                 deafaultChannel = channel;
                 channel.createMessage(contents.substring(prefix.length()));
