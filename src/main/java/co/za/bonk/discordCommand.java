942f8e4f81bc28071a284bcb6e5de11fac20650b
Udo <utme315@gmail.com>
2021-04-01 17:37:13 +0200

    gud

--- src/main/java/co/za/bonk/discordCommand.java
+++ src/main/java/co/za/bonk/discordCommand.java
@@ -5,15 +5,31 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.EventHandler;
 
-public class discordCommand implements CommandExecutor{
+public class DiscordCommand implements CommandExecutor{
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
         if (sender instanceof Player){
-            Player player = (Player) sender;
-            
-            
+            class CodeListen implements Listener{
+                @SuppressWarnings("deprecation")
+                @EventHandler
+                public void onPlayerChat(AsyncPlayerChatEvent event) {
+                    Player player = event.getPlayer();
+                    String message = event.getMessage();
+                    try{
+                        DiscordPaperPlugin.getStatement().executeUpdate("INSERT INTO discord (Minecraft_names) VALUES ("+player+") WHERE Reference_num = "+message+";");
+                    } catch(SQLException exception){
+                        exception.getStackTrace();
+                    }
+                    
+                }    
+                
+            }  
+   
         }
 
         return true;
