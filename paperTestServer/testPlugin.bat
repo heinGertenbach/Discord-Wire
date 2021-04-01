d804829e29a9449af1b3bf72224dfc7fe3078b29
heinGertenbach <71070518+heinGertenbach@users.noreply.github.com>
2021-04-01 14:26:04 +0200

    fixed jar dependancies

--- paperTestServer/testPlugin.bat
+++ paperTestServer/testPlugin.bat
@@ -1,5 +1,6 @@
 @echo off
-call mvn package
+call mvn clean compile assembly:single
+rem call mvn install
 copy /y "target\*.jar" "paperTestServer\plugins"
 cd ./paperTestServer
 java -Xmx1024m -Xms1024m -jar paper.jar nogui
