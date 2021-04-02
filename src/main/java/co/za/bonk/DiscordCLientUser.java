bbb11e8d452915e9064dc368ca69d0a381fff4de
heinGertenbach <71070518+heinGertenbach@users.noreply.github.com>
2021-04-02 14:31:59 +0200

    discord command handler

--- src/main/java/co/za/bonk/DiscordCLientUser.java
+++ src/main/java/co/za/bonk/DiscordCLientUser.java
@@ -1,20 +1,56 @@
 package co.za.bonk;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.object.entity.User;
 import discord4j.rest.entity.RestChannel;
 import discord4j.common.util.Snowflake;
+
+
+//Interface for command handling
+interface Command {
+    void execute(MessageCreateEvent event);
+}
+
 
 public class DiscordCLientUser implements Runnable {
 
     private static DiscordCLientUser instance;
     private static RestChannel deafaultChannel;
     private static GatewayDiscordClient gateway;
+
+    private static final Map<String, Command> commands = new HashMap<>();
+
+    //All discord commadns:
+
+    //test command
+    static {
+        commands.put("test", event -> {
+            event.getMessage().getChannel().block()
+            .createMessage("working!").block();
+        });
+    }
+
+    //register command for linking minecraft and discord:
+    static {
+        commands.put("register", event -> {
+            User author = event.getMessage().getAuthor().get();
+
+            //do SQL insertion for hash:
+            int hash = (author.getUsername() + author.getDiscriminator()).hashCode();
+
+
+            //return the command to enter to dm:
+            author.getPrivateChannel().block().createMessage("Enter command: /discord register " + hash).block();
+
+        });
+    }
     
     @Override
     public void run() {
@@ -30,14 +66,22 @@ public class DiscordCLientUser implements Runnable {
 
         deafaultChannel = client.getChannelById(Snowflake.of(plugin.getConfig().getString("discordConfig.deafaultChannel")));
 
-        gateway.on(MessageCreateEvent.class).subscribe(event -> {
-            final Message message = event.getMessage();
-            final String contents = message.getContent();
+        gateway.on(ReadyEvent.class).subscribe(event -> {
+            final User self = event.getSelf();
+            DiscordPaperPlugin.getInstance().getLogger().info("Discord Bot logged in as: " + self.getUsername() + "$" + self.getDiscriminator());
+        });
 
-            if(contents.startsWith("$") && message.getAuthor().map(user -> !user.isBot()).orElse(false)) {
-                final MessageChannel channel = message.getChannel().block();
-                channel.createMessage(contents.substring(prefix.length())).subscribe();
-            }
+        //register message listener:
+        gateway.on(MessageCreateEvent.class)
+            .subscribe(event -> {
+                final String contents = event.getMessage().getContent();
+
+                for (final Map.Entry<String, Command> entry : commands.entrySet()) {
+                    if (contents.startsWith(prefix + entry.getKey())) {
+                        entry.getValue().execute(event);
+                        break;
+                    }
+                }
         });
 
         gateway.onDisconnect().block();
