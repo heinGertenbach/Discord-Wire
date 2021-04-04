f5249cb941c2ffc8c9f96580eb4de3518ef3779b
heinGertenbach <71070518+heinGertenbach@users.noreply.github.com>
2021-04-04 20:13:22 +0200

    Update

--- src/main/java/co/za/bonk/discordCommand.java
+++ src/main/java/co/za/bonk/discordCommand.java
@@ -5,9 +5,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.EventHandler;
+// import org.bukkit.event.Listener;
+// import org.bukkit.event.player.AsyncPlayerChatEvent;
+// import org.bukkit.event.EventHandler;
 
 public class DiscordCommand implements CommandExecutor{
     
