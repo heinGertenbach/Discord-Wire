64b97207a500e98c1ecad9147334e6575cbbf37d
heinGertenbach <71070518+heinGertenbach@users.noreply.github.com>
2021-05-04 21:54:59 +0200

    discord name given

--- src/main/java/co/za/bonk/Database.java
+++ src/main/java/co/za/bonk/Database.java
@@ -4,5 +4,6 @@ public abstract class Database {
 
     public abstract String NewFromDiscord(String discordName) throws Exception;
     public abstract int Register(String uniqueHash, String minecraftName, String minecraftUUID) throws Exception;
+    public abstract String fromMinecraft(String minecraftHash) throws Exception;
 
 }
--- src/main/java/co/za/bonk/EventListener.java
+++ src/main/java/co/za/bonk/EventListener.java
@@ -12,10 +12,24 @@ public class EventListener implements Listener {
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
+        String playerHash = Hashing.hashString(player.getDisplayName());
+        Database database = DiscordPaperPlugin.getDatabase();
+
         String message = event.getMessage();
-        
         RestChannel channel = DiscordCLientUser.getDeafaultChannel();
 
-        channel.createMessage(player.getName() + ": " + message).block();
+        try {
+            String discordName = database.fromMinecraft(playerHash);
+            if (discordName != null) {
+                channel.createMessage(discordName.substring(0, discordName.length()-5) +": "+ message).block();
+                return;
+            }
+
+        } catch (Exception e) {
+            DiscordPaperPlugin.getInstance().getLogger().warning(e.toString());
+        }
+
+        //Send message without discord name
+        channel.createMessage(player.getName() +": "+ message).block();
     }
 }
