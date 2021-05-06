abe41d4f04b0c5b6c5a7a724e991136d624d9ea8
heinGertenbach <71070518+heinGertenbach@users.noreply.github.com>
2021-05-06 15:21:07 +0200

    removed server

--- paperTestServer/testPlugin.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call mvn clean compile assembly:single
-rem call mvn install
-copy /y "target\*.jar" "paperTestServer\plugins"
-cd ./paperTestServer
-java -Xmx1024m -Xms1024m -jar paper.jar nogui
